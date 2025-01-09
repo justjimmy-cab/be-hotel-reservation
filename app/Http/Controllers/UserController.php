710550bf2f27ae47d9b908631f07cea4ad45fef8
kiancataquiz <kiancataquiz18@gmail.com>
2025-01-09 19:15:53 +0800

    Created updateRole function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use app\Models\User;
 
 class UserController extends Controller
 {
@@ -16,58 +17,20 @@ class UserController extends Controller
 
                 // Return the paginated response
                 return response()->json($users);
-            
-        
-        
-        
-        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function updateRole(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404); 
+        }
+
+        $user->role = $request->input('role');
+        $user->save();
+
+        return response()->json(['message' => 'User role updated successfully', 'user' => $user], 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
