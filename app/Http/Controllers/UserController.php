694ec0c68d356654dbaca4889633c4009eb5730e
kiancataquiz <kiancataquiz18@gmail.com>
2025-01-09 20:28:37 +0800

    Created destroy function for user deletion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,4 +33,15 @@ class UserController extends Controller
         return response()->json(['message' => 'User role updated successfully', 'user' => $user], 200);
     }
 
+    public function destroy($id) { 
+        $user = User::find($id); 
+
+        if (!$user) { 
+            return response()->json(['message' => 'User not found'], 404); 
+        } 
+        
+        $user->delete(); 
+        return response()->json(['message' => 'User deleted successfully'], 200); 
+    }
+
 }
