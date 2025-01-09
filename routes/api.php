9c9a80c8c23fd0b7a63660b7fb1a6a7c61db5eb2
kiancataquiz <kiancataquiz18@gmail.com>
2025-01-09 19:20:23 +0800

    Added route for Role Update Requests

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,9 +14,13 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->group(function () {
+
+});
+
+// Admin-specific routes
+Route::middleware('role:admin')->group(function () {
     Route::get('/users', function () {
         return \App\Models\User::paginate(10);
     });
+    Route::put('/users/{id}/role', [UserController::class, 'updateRole']);
 });
-
-
